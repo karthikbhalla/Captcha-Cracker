5e878c3e95287e45d6557cb15e064646863ba8c4
Prasanth Louis <prasanthlouis21@gmail.com>
2015-02-01 22:47:54 +0530

    Update floodfill.py

--- floodfill.py
+++ floodfill.py
@@ -5,15 +5,21 @@ import shutil
 import sys
 
 sys.setrecursionlimit(10000)
-im2=Image.open("outputcrop15.gif")
+finalx=0
+finaly=0
 
 def floodfill(x, y, oldColor, newColor):
     # assume surface is a 2D image and surface[x][y] is the color at x, y.
+
 	pix=im2.getpixel((y,x))
 	if (pix!= oldColor):
+		global finalx
+		finalx=x
+		global finaly
+		finaly=y
 		return
 	im2.putpixel((y,x),newColor)
-	print 'hi'
+
 	try:
 		floodfill(x + 1, y, oldColor, newColor) # right
 		floodfill(x - 1, y, oldColor, newColor) # left
@@ -25,9 +31,15 @@ def floodfill(x, y, oldColor, newColor):
 flag=0	
 xcord=0
 ycord=0
+xcount=2
+im2=Image.open("outputcrop15.gif")
 for y in range(im2.size[0]):
 	if flag==1:
-		break
+		floodfill(xcord,ycord,0,255)
+		flag=0
+		if xcount>1:
+			im2.save("exp"+str(xcount)+".gif")
+			xcount=xcount-1
 	for x in range(im2.size[1]):
 		pix = im2.getpixel((y,x))
 		if pix!=255:
@@ -36,6 +48,23 @@ for y in range(im2.size[0]):
 			flag=1
 			break
 			
-floodfill(xcord,ycord,0,255)
-im2.save("exp.gif")
+im2=Image.open("outputcrop15.gif")
+for y in range(finaly,im2.size[0]):
+	if flag==1:
+		floodfill(xcord,ycord,0,255)
+		flag=0
+		im2.save("exp"+str(xcount)+".gif")
+		print finalx
+		print finaly
+		xcount=xcount+1
+	for x in range(finalx,im2.size[1]):
+		pix = im2.getpixel((y,x))
+		if pix!=255:
+			xcord=x
+			ycord=y
+			flag=1
+			break
+			
+
+
 
